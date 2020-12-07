6d8a86adcd2c34b5b50e7898a332f7989c997345
Mjsilva7 <mj.silva7@gmail.com>
2020-12-06 21:06:57 -0300

    meu primeiro commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:app_loja/tabs/home_tab.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        HomeTab(),
+      ],
+    );
+  }
+}
