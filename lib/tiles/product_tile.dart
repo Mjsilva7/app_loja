0230872e1cdbc1d39249597b152e042b15779743
Mjsilva7 <mj.silva7@gmail.com>
2020-12-11 17:46:45 -0300

    Novas implementações

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,4 +1,5 @@
 import 'package:app_loja/datas/product_data.dart';
+import 'package:app_loja/screens/product_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProductTile extends StatelessWidget {
@@ -9,38 +10,79 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => ProductScreen(product)),
+        );
+      },
       child: Card(
         child: type == "grid"
             ? Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                    AspectRatio(
-                        aspectRatio: 0.8,
-                        child: Image.network(
-                          product.images[0],
-                          fit: BoxFit.cover,
-                        )),
-                    Expanded(
-                      child: Container(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Text(
-                                product.title,
-                                style: TextStyle(fontWeight: FontWeight.w500),
-                              ),
-                              Text("R\$ ${product.price.toStringAsFixed(2)}",
-                                  style: TextStyle(
-                                    color: Theme.of(context).primaryColor,
-                                    fontSize: 17.0,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                            ],
-                          )),
-                    )
-                  ])
-            : Row(),
+                  AspectRatio(
+                      aspectRatio: 0.8,
+                      child: Image.network(
+                        product.images[0],
+                        fit: BoxFit.cover,
+                      )),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "R\$ ${product.price.toStringAsFixed(2)}",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Flexible(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "R\$ ${product.price.toStringAsFixed(2)}",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
