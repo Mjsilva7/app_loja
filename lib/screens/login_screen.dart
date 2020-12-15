ecdb96b59cad48b3f8b5819978122119054ebebf
Mjsilva7 <mj.silva7@gmail.com>
2020-12-15 15:56:55 -0300

    Novas funcionalidades

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,12 +3,23 @@ import 'package:app_loja/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+
+  final _passController = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Entrar"),
         centerTitle: true,
@@ -39,6 +50,7 @@ class LoginScreen extends StatelessWidget {
             padding: EdgeInsets.all(16),
             children: [
               TextFormField(
+                controller: _emailController,
                 decoration: InputDecoration(hintText: "E-mail"),
                 keyboardType: TextInputType.emailAddress,
                 validator: (text) {
@@ -48,6 +60,7 @@ class LoginScreen extends StatelessWidget {
               ),
               SizedBox(height: 16),
               TextFormField(
+                controller: _passController,
                 decoration: InputDecoration(hintText: "Senha"),
                 obscureText: true,
                 validator: (text) {
@@ -57,7 +70,26 @@ class LoginScreen extends StatelessWidget {
               Align(
                 alignment: Alignment.centerRight,
                 child: FlatButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    if (_emailController.text.isEmpty)
+                      _scaffoldKey.currentState.showSnackBar(
+                        SnackBar(
+                          content: Text("Insira seu e-mail para recuperação!"),
+                          backgroundColor: Colors.redAccent,
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                    else {
+                      model.recovePass(_emailController.text);
+                      _scaffoldKey.currentState.showSnackBar(
+                        SnackBar(
+                          content: Text("Confira seu e-mail!"),
+                          backgroundColor: Theme.of(context).primaryColor,
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                    }
+                  },
                   child: Text(
                     "Esqueci minha senha",
                     textAlign: TextAlign.right,
@@ -77,7 +109,12 @@ class LoginScreen extends StatelessWidget {
                   color: Theme.of(context).primaryColor,
                   onPressed: () {
                     if (_formKey.currentState.validate()) {}
-                    model.signIn();
+                    model.signIn(
+                      email: _emailController.text,
+                      pass: _passController.text,
+                      onSuccess: _onSuccess,
+                      onFail: _onFail,
+                    );
                   },
                 ),
               ),
@@ -85,6 +122,20 @@ class LoginScreen extends StatelessWidget {
           ),
         );
       }),
+    );
+  }
+
+  void _onSuccess() {
+    Navigator.of(context).pop();
+  }
+
+  void _onFail() {
+    _scaffoldKey.currentState.showSnackBar(
+      SnackBar(
+        content: Text("Falha ao entrar!"),
+        backgroundColor: Colors.redAccent,
+        duration: Duration(seconds: 2),
+      ),
     );
   }
 }
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:app_loja/models/user_model.dart';
 import 'package:app_loja/screens/login_screen.dart';
 import 'package:app_loja/tiles/drawer_tile.dart';
 import 'package:flutter/material.dart';
@@ -47,31 +48,43 @@ class CustomDrawer extends StatelessWidget {
                     Positioned(
                       left: 0.0,
                       bottom: 0.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Olá",
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          GestureDetector(
-                            child: Text(
-                              "Entre ou cadastre-se >",
-                              style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
+                      child: ScopedModelDescendant<UserModel>(
+                        builder: (context, child, model) {
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Olá, ${!model.isLoggedIn() ? "" : model.userData["name"]}",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                            onTap: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => LoginScreen()));
-                            },
-                          ),
-                        ],
+                              GestureDetector(
+                                child: Text(
+                                  !model.isLoggedIn()
+                                      ? "Entre ou cadastre-se >"
+                                      : "Sair",
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                onTap: () {
+                                  if (!model.isLoggedIn())
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) => LoginScreen(),
+                                      ),
+                                    );
+                                  else
+                                    model.signOut();
+                                },
+                              ),
+                            ],
+                          );
+                        },
                       ),
                     )
                   ],
